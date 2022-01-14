5ad694cbbd79d703422836c730c1c32d0f069610
douglasddrdias <douglasddrdias@gmail.com>
2022-01-14 13:52:48 -0300

    Materiais do projeto starflix

--- js/apiFilmeUtil.js
+++ js/apiFilmeUtil.js
@@ -1,15 +1,12 @@
-// const alterarBackgroundComImagemApi = async (div) => {
-//     let imagemApi = await recuperarAndSelecionarAleatoriamenteImagemBackgroud();
-//     alterarBackground(div, URL_BACKGROUND + imagemApi);
-// }
-
 const CHAVE = 'api_key=71e536b40e46d8ed840cb40077454756';
 const URL_BASE_API = 'https://api.themoviedb.org/3/';
 const URL_FILME = 'movie/'
 const LINGUAGEM_PT_BR = '&language=pt-br';
-const URL_BACKGROUND = 'https://www.themoviedb.org/t/p/original';
+const URL_BASE_IMAGEM = 'https://www.themoviedb.org/t/p/';
+const URL_BACKGROUND = URL_BASE_IMAGEM + 'original';
 const URL_BUSCA_IMAGENS = '/images'
-const URL_POSTER ='https://www.themoviedb.org/t/p/w600_and_h900_bestv2';
+const URL_POSTER = URL_BASE_IMAGEM + 'w600_and_h900_bestv2';
+const URL_BUSCA_VIDEOS = '/videos';
 
 // recupera as imagens relacionadas ao filme selecionado
 async function recuperarImagensFilmeNaApi (id) {
@@ -39,11 +36,41 @@ async function realizarAcessoApi (url){
 }
 
 // recupera uma imagem para background 
-const recuperarAndSelecionarAleatoriamenteImagemBackgroud= async () =>{
-    let imagensApi = await recuperarImagensFilmeNaApi(objetoAtualApi.id);
+async function recuperarAndSelecionarAleatoriamenteImagemBackgroud(objetoApi){
+    let imagensApi = await recuperarImagensFilmeNaApi(objetoApi.id);
     if (!imagensApi || !imagensApi.backdrops || imagensApi.backdrops.length <= 0){
-        return objetoAtualApi.backdrop_path;
+        return objetoApi.backdrop_path;
     }
     let numeroAleatorio = getNumeroAleatorio(imagensApi.backdrops.length -1);
     return URL_BACKGROUND + imagensApi.backdrops[numeroAleatorio].file_path;
+}
+
+// recupera os videos associados ao filme caso não ache um em pt-br refaz a busca sem liguagem associada
+async function recuperarVideoYoutube(objetoApi){
+    let videos = await recuperarVideosNaApi(objetoApi.id, LINGUAGEM_PT_BR);
+    let videoYoutube = recuperarPrimeiroVideoYoutube(videos);
+    if (!videoYoutube){
+        videos = await recuperarVideosNaApi(objetoApi.id);
+        videoYoutube = recuperarPrimeiroVideoYoutube(videos);
+    }
+    return videoYoutube;
+}
+
+// recupera o primeiro video com site igual a youtube 
+async function recuperarPrimeiroVideoYoutube(videos){
+    if (!videos || !videos.result || videos.result.length <= 0){
+        return false;
+    }
+    for (v of videos.result){
+        if (v.site === "YouTube"){
+            return v;
+        }
+    }
+    return false;
+}
+
+// recupera os videos associados ao filme
+async function recuperarVideosNaApi(id, linguagem = ''){
+    let resposta = await realizarAcessoApi(URL_BASE_API+URL_FILME+id+URL_BUSCA_VIDEOS+'?'+CHAVE + linguagem);
+    return resposta;    
 }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 
 class Filme {
-    constructor(urlTrailerYoutube, idFilmeSwapi){
+    constructor(urlTrailerYoutube, idFilmeSwapi) {
         this.urlTrailerYoutube = urlTrailerYoutube;
         this.idFilmeSwapi = idFilmeSwapi;
     }
-}
-const assistirTrailerYoutube = () => {
-
 }
 // mais informações
 const lblTituloMaisInformacoes = document.getElementById('mais-informacoes-titulo');
@@ -26,28 +23,30 @@ let objetoAtual;
 let arrayNomeImg;
 let nomeImg;
 let objetoAtualApi;
+const URL_VIDEO_YOUTUBE = 'https://www.youtube.com/embed/';
+let idVideoYoutube;
 const divListaFilmes = document.getElementById('listaFilmes');
-const arrayFilmes = [new Filme('https://www.youtube.com/embed/jVrf_bKTjo4','11'), 
-                     new Filme('https://www.youtube.com/embed/bD7bpG-zDJQ', '1893'),
-                     new Filme('https://www.youtube.com/embed/-di3XYRxyHY', '181808'),
-                     new Filme('https://www.youtube.com/embed/yMglylP5xhA', '140607'),
-                     new Filme('https://www.youtube.com/embed/jiRTfUYOHCs', '181812'),
-                     new Filme('https://www.youtube.com/embed/n7DQ9SdjUmg', '1894'),
-                     new Filme('https://www.youtube.com/embed/znpliCR0_Kw', '1895'), 
-                     new Filme('https://www.youtube.com/embed/4KnAyxgZ3Vo', '1891'),
-                     new Filme('https://www.youtube.com/embed/BT-fVW24-q4', '1892'),
-                     new Filme('https://www.youtube.com/embed/EQpr5HXTvmg', '348350'),
-                     new Filme('https://www.youtube.com/embed/9oISQcXuki0', '330459')  
-                    ];
+const arrayFilmes = [new Filme('jVrf_bKTjo4', '11'),
+new Filme('bD7bpG-zDJQ', '1893'),
+new Filme('-di3XYRxyHY', '181808'),
+new Filme('yMglylP5xhA', '140607'),
+new Filme('jiRTfUYOHCs', '181812'),
+new Filme('n7DQ9SdjUmg', '1894'),
+new Filme('znpliCR0_Kw', '1895'),
+new Filme('4KnAyxgZ3Vo', '1891'),
+new Filme('BT-fVW24-q4', '1892'),
+new Filme('EQpr5HXTvmg', '348350'),
+new Filme('9oISQcXuki0', '330459')
+];
 
 
 // altera o filme selecionado
-async function  alterarFilmeDestaque(objetoApi) {
+async function alterarFilmeDestaque(objetoApi) {
     if (!arrastouItem) {
         objetoAtualApi = objetoApi;
         await alterarBackgroundComImagemApi(divFilmePrincipal);
         divFilmePrincipal.classList.add('filme-principal-responsivo');
-        divFilmePrincipal.style.height= '500px'
+        divFilmePrincipal.style.height = '500px'
         alterarTituloFilmeDestaque();
         alterarDescricaoFilmeDestaque();
         alterarVisibilidadeBotoes();
@@ -55,44 +54,44 @@ async function  alterarFilmeDestaque(objetoApi) {
 }
 
 // Altera o titulo do filme destaque
-function alterarTituloFilmeDestaque(){
+function alterarTituloFilmeDestaque() {
     adicionarMensagemAoLabel(lblTitulo, objetoAtualApi.title);
     lblTitulo.classList.add("titulo")
 }
 
 // altera a descrição do filme destaque
-function alterarDescricaoFilmeDestaque(){
+function alterarDescricaoFilmeDestaque() {
     adicionarMensagemAoLabel(lblDescricao, objetoAtualApi.overview);
 }
 // altera visibilidade dos botões de assistir e mais informações
-function alterarVisibilidadeBotoes(){
+function alterarVisibilidadeBotoes() {
     alterarVisibilidadeBotao(btnAssistir);
     alterarVisibilidadeBotao(btnMaisInformacoes);
 }
 
 // carrega os eventos da página
-async function carregarEventosAndInformacoesPadrao() { 
+async function carregarEventosAndInformacoesPadrao() {
     btnAssistir.addEventListener("click", assistirTrailerYoutube);
-    btnMaisInformacoes.addEventListener('click', recuperarMaisInformacoesFilme); 
-    for (filme of arrayFilmes){
+    btnMaisInformacoes.addEventListener('click', recuperarMaisInformacoesFilme);
+    for (filme of arrayFilmes) {
         let objetoApi = await recuperarMaisInformacoesFilmeNaApi(filme.idFilmeSwapi);
         carregarItemFilmesOwl(objetoApi);
     }
     carregarOwlCarousel('#listaFilmes');
 }
 
-function carregarItemFilmesOwl (objetoApi) {
+function carregarItemFilmesOwl(objetoApi) {
     let divItem = criarDivItemOwl();
     let imagem = criarImgItemOwl(URL_POSTER + objetoApi.poster_path);
-    imagem.addEventListener('click', function (){
+    imagem.addEventListener('click', function () {
         alterarFilmeDestaque(objetoApi);
     })
     divItem.appendChild(imagem);
-    divListaFilmes.appendChild(divItem);   
+    divListaFilmes.appendChild(divItem);
 }
 
 // Altera as informações do modal de mais informações
-async function recuperarMaisInformacoesFilme () {
+async function recuperarMaisInformacoesFilme() {
     adicionarMensagemAoLabel(lblTituloMaisInformacoes, objetoAtualApi.title);
     adicionarMensagemAoLabel(lblTituloOriginal, objetoAtualApi.original_title);
     adicionarMensagemAoLabel(lblDataLancamento, formatarData(objetoAtualApi.release_date));
@@ -100,23 +99,46 @@ async function recuperarMaisInformacoesFilme () {
     await alterarBackgroundComImagemApi(divCorpoMaisInformacoes);
 }
 
-async function alterarBackgroundComImagemApi (div) {
-    let imagemApi = await recuperarAndSelecionarAleatoriamenteImagemBackgroud();
+// altera o background do filme com imagem da api
+async function alterarBackgroundComImagemApi(div) {
+    let imagemApi = await recuperarAndSelecionarAleatoriamenteImagemBackgroud(objetoAtualApi);
     alterarBackground(div, imagemApi);
 }
-  
-$(document).ready(function() {
+
+// recupera o idVideoYoutube caso não seja encontrado nenhum é recuperado do array
+async function assistirTrailerYoutube() {
+    video = await recuperarVideoYoutube(objetoAtualApi);
+    if (!video || !video.key) {
+        idVideoYoutube = recuperarIdYoutubeArray(objetoAtualApi.id);
+    } else {
+        idVideoYoutube = video.key;
+        console.log('id no assistir trailer ', idVideoYoutube);
+    }
+    
+}
+
+// recupera o idVideoYoutube para o id passado 
+function recuperarIdYoutubeArray(id) {
+    for (f of arrayFilmes) {
+        if (f.idFilmeSwapi == id) {
+            return f.urlTrailerYoutube;
+        }
+    }
+}
+
+$(document).ready(function () {
     // when the modal is opened autoplay it  
-    $('#myModal').on('shown.bs.modal', function (e) {        
-    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-    $("#video").attr('src', objetoAtual.urlTrailerYoutube + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+    $('#myModal').on('shown.bs.modal', function (e) {
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        console.log(URL_VIDEO_YOUTUBE + idVideoYoutube + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        $("#video").attr('src', URL_VIDEO_YOUTUBE + idVideoYoutube + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
     })
     // stop playing the youtube video when I close the modal
     $('#myModal').on('hide.bs.modal', function (e) {
         // a poor man's stop video
-        $("#video").attr('src',''); 
-    }) 
-// document ready  
+        $("#video").attr('src', '');
+    })
+    // document ready  
 });
 
 carregarEventosAndInformacoesPadrao();
