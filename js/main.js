08776ef8c81034f6f39c9aaba1e57debb3459eda
douglasddrdias <douglasddrdias@gmail.com>
2022-01-14 00:17:15 -0300

    Materiais do projeto starflix

--- js/main.js
+++ js/main.js
@@ -1,26 +1,6 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    video:true,
-    autoHeight:true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
-        }
-    }
-})
+
 class Filme {
-    constructor(imgBack, descricao, titulo, urlTrailerYoutube, idFilmeSwapi){
-        this.imgBack = imgBack;
-        this.descricao = descricao;
-        this.titulo = titulo;
+    constructor(urlTrailerYoutube, idFilmeSwapi){
         this.urlTrailerYoutube = urlTrailerYoutube;
         this.idFilmeSwapi = idFilmeSwapi;
     }
@@ -48,39 +28,37 @@ const URL_FILME = 'movie/'
 const LINGUAGEM_PT_BR = '&language=pt-br';
 const URL_BACKGROUND = 'https://www.themoviedb.org/t/p/original';
 const URL_BUSCA_IMAGENS = '/images'
+const URL_POSTER ='https://www.themoviedb.org/t/p/w600_and_h900_bestv2';
 let objetoAtual;
 let arrayNomeImg;
 let nomeImg;
 let objetoAtualApi;
+const divListaFilmes = document.getElementById('listaFilmes');
 const mapFilmes = new Map([
-    ["star-wars-nova-esperanca.jpg",new Filme('star-wars-nova-esperanca-capa.jpg','A princesa Leia é mantida refém pelas forças imperiais comandadas por Darth Vader. Luke Skywalker e Han Solo precisam libertá-la e restaurar a liberdade e a justiça na galáxia.', 'STAR WARS - Episódio IV - Uma nova esperança','https://www.youtube.com/embed/jVrf_bKTjo4','11')        
-    ],
-    ["star-wars-a-ameaca-fantasma.jpg", new Filme('star-wars-a-ameaca-fantasma-capa.jpg', 'Enquanto resgatam a Rainha Amidala, governante de um planeta pacífico invadido por forças ameaçadoras, os Cavaleiros Jedi Obi-Wan Kenobi e Qui-Gon Jinn descobrem Anakin Skywalker, uma criança prodígio de 9 anos excepcionalmente poderosa na Força.', 'STAR WARS - Episódio I - A Ameaça Fantasma', 'https://www.youtube.com/embed/bD7bpG-zDJQ', '1893')
-    ],
-    ["star-wars-ultimos-jedi.jpg", new Filme('star-wars-ultimos-jedi-capa.jpg', 'A tranquila e solitária vida de Luke Skywalker sofre uma reviravolta quando ele conhece Rey, uma jovem que mostra fortes sinais da Força. O desejo dela de aprender o estilo dos Jedi força Luke a tomar uma decisão que mudará sua vida para sempre. Enquanto isso, Kylo Ren e o General Hux lideram a Primeira Ordem para um ataque total contra Leia e a Resistência pela supremacia da galáxia.', 'STAR WARS - Episódio VIII - Os Últimos Jedi', 'https://www.youtube.com/embed/-di3XYRxyHY', '181808')
-    ],
-    ["star-wars-despertar-da-forca.jpg", new Filme('star-wars-despertar-da-forca-capa.jpg', 'A queda de Darth Vader e do Império levou ao surgimento de uma nova força sombria: a Primeira Ordem. Eles procuram o jedi Luke Skywalker, desaparecido. A resistência tenta desesperadamente encontrá-lo antes para salvar a galáxia.', 'STAR WARS - Episódio VII - O Despertar da Força', 'https://www.youtube.com/embed/yMglylP5xhA', '140607')
-    ],
-    ["star-wars-a-ascensao-skywalker.jpg", new Filme('star-wars-a-ascensao-skywalker-capa.jpg', 'Com o retorno do Imperador Palpatine, todos voltam a temer seu poder e, com isso, a Resistência toma a frente da batalha que ditará os rumos da galáxia. Treinando para ser uma completa Jedi, Rey (Daisy Ridley) ainda se encontra em conflito com seu passado e futuro, mas teme pelas respostas que pode conseguir a partir de sua complexa ligação com Kylo Ren (Adam Driver), que também se encontra em conflito pela Força.', 'STAR WARS - Episódio IX - Ascensão Skywalker', 'https://www.youtube.com/embed/jiRTfUYOHCs', '181812')
-    ],
-    ["star-wars-ataque-dos-clones.jpg", new Filme('star-wars-ataque-dos-clones-capa.jpg', 'Com a missão de proteger a Senadora Amidala, Anakin Skywalker descobre o seu amor por ela, do mesmo modo que vai conhecendo seu lado sombrio. Obi-Wan Kenobi descobre um exército de clones à medida que a Galáxia caminha para uma grande guerra.', 'STAR WARS - Episódio II - Ataque dos Clones', 'https://www.youtube.com/embed/n7DQ9SdjUmg', '1894')
-    ]
-    ,
-    ["star-wars-a-vinganca-dos-sith.jpg", new Filme('star-wars-a-vinganca-dos-sith-capa.jpg', 'As Guerras Clônicas estão em pleno andamento e Anakin Skywalker mantém um elo de lealdade com Palpatine, ao mesmo tempo em que luta para que seu casamento com Padmé Amidala não seja afetado por esta situação. Seduzido por promessas de poder, Anakin se aproxima cada vez mais de Darth Sidious até se tornar o temível Darth Vader. Juntos eles tramam um plano para aniquilar de uma vez por todas com os cavaleiros jedi.', 'STAR WARS - Episódio III - A Vingança dos Sith', 'https://www.youtube.com/embed/znpliCR0_Kw', '1895')
-    ]       
-    ,
-    ["star-wars-imperio-contra-ataca.jpg", new Filme('star-wars-imperio-contra-ataca-capa.jpg', 'As forças imperais comandadas por Darth Vader (David Prowse) lançam um ataque contra os membros da resistência, que são obrigados a fugir. Enquanto isso Luke Skywalker (Mark Hamill) tenta encontrar o Mestre Yoda, que poderá ensiná-lo a dominar a "Força" e torná-lo um cavaleiro jedi. No entanto, Darth Vader planeja levá-lo para o lado negro da "Força".', 'STAR WARS - Episódio V - O Império Contra-Ataca', 'https://www.youtube.com/embed/4KnAyxgZ3Vo', '1891')
-    ]    
-    ,
-    ["star-wars-retorno-jedi.jpg", new Filme('star-wars-retorno-jedi-capa.jpg', 'O imperador está supervisionando a construção de uma nova Estrela da Morte. Enquanto isso Luke Skywalker liberta Han Solo e a Princesa Leia das mãos de Jaba. Luke só se tornará um cavaleiro jedi quando destruir Darth Vader, que ainda pretende atraí-lo para o lado sombrio da Força.', 'STAR WARS - Episódio VI - O Retorno de Jedi', 'https://www.youtube.com/embed/BT-fVW24-q4', '1892')
-    ]    
-    ,
-    ["star-wars-han-solo.jpg", new Filme('star-wars-han-solo-capa.jpg', 'Embarque na Millennium Falcon e viaje para uma galáxia distante em uma nova aventura com o mais amado canalha da galáxia. Através de uma série de ousadas aventuras no obscuro e perigoso submundo do crime, Han Solo encontra seu poderoso futuro copiloto Chewbacca e encontra o famoso jogador Lando Calrissian, em uma jornada que definirá o curso de um dos heróis mais improváveis da saga Star Wars.', 'Han Solo: Uma História Star Wars', 'https://www.youtube.com/embed/EQpr5HXTvmg', '348350')
-    ]    
-    ,
-    ["star-wars-rogue-one.jpg", new Filme('star-wars-rogue-one-capa.jpg', 'Em busca de uma nova esperança para a galáxia, a Aliança Rebelde enfrenta uma arriscada missão: roubar os planos que revelam uma falha na construção da Estrela da Morte. Para isso, eles contam com a ajuda de Jyn Erso, filha do criador da arma mais poderosa do império.', 'Rogue One: Uma História Star Wars', 'https://www.youtube.com/embed/9oISQcXuki0', '330459')
-    ]    
+    ["star-wars-nova-esperanca.jpg",new Filme('https://www.youtube.com/embed/jVrf_bKTjo4','11')],
+    ["star-wars-a-ameaca-fantasma.jpg", new Filme('https://www.youtube.com/embed/bD7bpG-zDJQ', '1893')],
+    ["star-wars-ultimos-jedi.jpg", new Filme('https://www.youtube.com/embed/-di3XYRxyHY', '181808')],
+    ["star-wars-despertar-da-forca.jpg", new Filme('https://www.youtube.com/embed/yMglylP5xhA', '140607')],
+    ["star-wars-a-ascensao-skywalker.jpg", new Filme('https://www.youtube.com/embed/jiRTfUYOHCs', '181812')],
+    ["star-wars-ataque-dos-clones.jpg", new Filme('https://www.youtube.com/embed/n7DQ9SdjUmg', '1894')],
+    ["star-wars-a-vinganca-dos-sith.jpg", new Filme('https://www.youtube.com/embed/znpliCR0_Kw', '1895')],
+    ["star-wars-imperio-contra-ataca.jpg", new Filme('https://www.youtube.com/embed/4KnAyxgZ3Vo', '1891')],
+    ["star-wars-retorno-jedi.jpg", new Filme('https://www.youtube.com/embed/BT-fVW24-q4', '1892')],
+    ["star-wars-han-solo.jpg", new Filme('https://www.youtube.com/embed/EQpr5HXTvmg', '348350')],
+    ["star-wars-rogue-one.jpg", new Filme('https://www.youtube.com/embed/9oISQcXuki0', '330459')]    
 ])
+const arrayFilmes = [new Filme('https://www.youtube.com/embed/jVrf_bKTjo4','11'), 
+                     new Filme('https://www.youtube.com/embed/bD7bpG-zDJQ', '1893'),
+                     new Filme('https://www.youtube.com/embed/-di3XYRxyHY', '181808'),
+                     new Filme('https://www.youtube.com/embed/yMglylP5xhA', '140607'),
+                     new Filme('https://www.youtube.com/embed/jiRTfUYOHCs', '181812'),
+                     new Filme('https://www.youtube.com/embed/n7DQ9SdjUmg', '1894'),
+                     new Filme('https://www.youtube.com/embed/znpliCR0_Kw', '1895'), 
+                     new Filme('https://www.youtube.com/embed/4KnAyxgZ3Vo', '1891'),
+                     new Filme('https://www.youtube.com/embed/BT-fVW24-q4', '1892'),
+                     new Filme('https://www.youtube.com/embed/EQpr5HXTvmg', '348350'),
+                     new Filme('https://www.youtube.com/embed/9oISQcXuki0', '330459')  
+                    ];
 // marreta para diferenciar clicar e arrastar
 let arrastouItem = false;
 document.addEventListener('mousedown', () => arrastouItem = false);
@@ -102,10 +80,10 @@ function limparLabel(lblResultado) {
 }
 
 // altera o filme selecionado
-function alterarFilmeDestaque(img) {
+async function  alterarFilmeDestaque(objetoApi) {
     if (!arrastouItem) {
-        recuperarObjetoAtual(img);
-        alterarBackground(divFilmePrincipal, `img/${objetoAtual.imgBack}`);
+        objetoAtualApi = objetoApi;
+        await alterarBackgroundComImagemApi(divFilmePrincipal);
         divFilmePrincipal.classList.add('filme-principal-responsivo');
         divFilmePrincipal.style.height= '500px'
         alterarTituloFilmeDestaque();
@@ -113,9 +91,7 @@ function alterarFilmeDestaque(img) {
         alterarVisibilidadeBotoes();
     }
 }
-function recuperarObjetoAtual(img) {
-    arrayNomeImg = img.src.split(`/`);
-    nomeImg = arrayNomeImg[arrayNomeImg.length - 1];
+function recuperarObjetoAtual(id) {
     objetoAtual = mapFilmes.get(nomeImg);
 }
 
@@ -128,13 +104,14 @@ function alterarBackground(div, img) {
 
 // Altera o titulo do filme destaque
 function alterarTituloFilmeDestaque(){
-    adicionarMensagemAoLabel(lblTitulo, objetoAtual.titulo);
+    // adicionarMensagemAoLabel(lblTitulo, objetoAtual.titulo);
+    adicionarMensagemAoLabel(lblTitulo, objetoAtualApi.title);
     lblTitulo.classList.add("titulo")
 }
 
 // altera a descrição do filme destaque
 function alterarDescricaoFilmeDestaque(){
-    adicionarMensagemAoLabel(lblDescricao, objetoAtual.descricao);
+    adicionarMensagemAoLabel(lblDescricao, objetoAtualApi.overview);
 }
 // altera visibilidade dos botões de assistir e mais informações
 function alterarVisibilidadeBotoes(){
@@ -143,40 +120,87 @@ function alterarVisibilidadeBotoes(){
 }
 
 // carrega os eventos da página
-function carregarEventosAndInformacoesPadrao() { 
+async function carregarEventosAndInformacoesPadrao() { 
     btnAssistir.addEventListener("click", assistirTrailerYoutube);
-    btnMaisInformacoes.addEventListener('click', recuperarMaisInformacoesFilme);
-    if (imagens != null && imagens.length > 0) {
-        for (let img of imagens) {
-            img.addEventListener('mouseup', function () {
-                alterarFilmeDestaque(img);
-            })
-        }
+    btnMaisInformacoes.addEventListener('click', recuperarMaisInformacoesFilme); 
+    for (filme of arrayFilmes){
+        let objetoApi = await recuperarMaisInformacoesFilmeNaApi(filme.idFilmeSwapi);
+        carregarItemFilmesOwl(objetoApi);
     }
+    carregarOwlCarousel();
+}
+
+
+// carrega o owl carousel na tela
+function carregarOwlCarousel(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })     
+}
+
+const carregarItemFilmesOwl = (objetoApi) => {
+    let divItem = criarDivItemOwl();
+    let imagem = criarImgItemOwl(URL_POSTER + objetoApi.poster_path);
+    imagem.addEventListener('click', function (){
+        alterarFilmeDestaque(objetoApi);
+    })
+    divItem.appendChild(imagem);
+    divListaFilmes.appendChild(divItem);   
+}
+
+function criarImgItemOwl(poster) {
+    let testImagem = document.createElement('img');
+    testImagem.classList.add('box-filme');
+    testImagem.src = poster;
+    return testImagem;
+}
+
+function criarDivItemOwl() {
+    let divItem = document.createElement('div');
+    divItem.classList.add('item');
+    return divItem;
 }
 
 // Altera as informações do modal de mais informações
 const recuperarMaisInformacoesFilme = async () => {
-    objetoAtualApi = await recuperarMaisInformacoesFilmeNaApi(objetoAtual.idFilmeSwapi);
+    await recuperarObjetoAtualApi();
     adicionarMensagemAoLabel(lblTituloMaisInformacoes, objetoAtualApi.title);
     adicionarMensagemAoLabel(lblTituloOriginal, objetoAtualApi.original_title);
     adicionarMensagemAoLabel(lblDataLancamento, formatarData(objetoAtualApi.release_date));
     adicionarMensagemAoLabel(lblNotaFilme, objetoAtualApi.vote_average);
-    await alterarBackgroundMaisInformacoes()
+    await alterarBackgroundComImagemApi(divCorpoMaisInformacoes);
 }
 
-const alterarBackgroundMaisInformacoes = async () => {
+const alterarBackgroundComImagemApi = async (div) => {
     let imagemApi = await recuperarAndSelecionarAleatoriamenteImagemBackgroud();
-    alterarBackground(divCorpoMaisInformacoes, URL_BACKGROUND + imagemApi);
+    alterarBackground(div, URL_BACKGROUND + imagemApi);
 }
 // recupera uma imagem para background 
 const recuperarAndSelecionarAleatoriamenteImagemBackgroud= async () =>{
-    let imagensApi = await recuperarImagensFilmeNaApi(objetoAtual.idFilmeSwapi);
+    let imagensApi = await recuperarImagensFilmeNaApi(objetoAtualApi.id);
     if (!imagensApi || !imagensApi.backdrops || imagensApi.backdrops.length <= 0){
         return objetoAtualApi.backdrop_path;
     }
     let numeroAleatorio = getNumeroAleatorio(imagensApi.backdrops.length -1);
     return imagensApi.backdrops[numeroAleatorio].file_path;
+}
+
+// recupera as informações do filme na api
+async function recuperarObjetoAtualApi() {
+    objetoAtualApi = await recuperarMaisInformacoesFilmeNaApi(objetoAtualApi.id);
 }
 
 // gera um número inteiro aleatorio até o valor máximo
@@ -235,3 +259,9 @@ $(document).ready(function() {
 });
 
 carregarEventosAndInformacoesPadrao();
+
+/*Poster
+https://www.themoviedb.org/t/p/w600_and_h900_bestv2/dLGT8b4Ut10z44uYLaip4QiwKta.jpg
+
+back
+https://www.themoviedb.org/t/p/original/aJCtkxLLzkk1pECehVjKHA2lBgw.jpg*/
